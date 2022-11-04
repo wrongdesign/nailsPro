8f52682c911f1966b64c3da01feb73bd3b7ad5c4
unknown <resthave375@gmail.com>
2022-11-04 15:37:13 +0300

    feat: add hamburger menu with all functions

--- script.js
+++ script.js
@@ -58,63 +58,80 @@ function changeColor(object) {
     let rgbColor = object.style.backgroundColor;
     let r = "", g = "", b = "";
     let k = 1;
-    for(let i = 4; i < rgbColor.length; i++) {
-        if(k == 1) {
+    for (let i = 4; i < rgbColor.length; i++) {
+        if (k == 1) {
             r = r + rgbColor[i];
-            if(rgbColor[i + 1] == ",") {
+            if (rgbColor[i + 1] == ",") {
                 i += 2;
                 k++;
             }
         }
 
-        if(k == 2) {
+        if (k == 2) {
             g = g + rgbColor[i];
-            if(rgbColor[i + 1] == ",") {
+            if (rgbColor[i + 1] == ",") {
                 i += 2;
                 k++;
             }
         }
 
-        if(k == 3) {
+        if (k == 3) {
             b = b + rgbColor[i];
-            if(rgbColor[i + 1] == ")") {
+            if (rgbColor[i + 1] == ")") {
                 break;
             }
         }
     }
 
-    let hexValues = new Array("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f");
-    let h = isNaN(r) ? "00" : hexValues[(r - r % 16) / 16] + hexValues[r % 16]; 
-    let e = isNaN(g) ? "00" : hexValues[(g - g % 16) / 16] + hexValues[g % 16]; 
+    let hexValues = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
+    let h = isNaN(r) ? "00" : hexValues[(r - r % 16) / 16] + hexValues[r % 16];
+    let e = isNaN(g) ? "00" : hexValues[(g - g % 16) / 16] + hexValues[g % 16];
     let x = isNaN(b) ? "00" : hexValues[(b - b % 16) / 16] + hexValues[b % 16];
-    
+
     let color = "#" + h + e + x;
 
-    return document.getElementById("nail1").style.backgroundColor = color, 
-    document.getElementById("nail2").style.backgroundColor = color, 
-    document.getElementById("nail3").style.backgroundColor = color,
-    document.getElementById("tile").style.backgroundColor = color;
+    return document.getElementById("nail1").style.backgroundColor = color,
+        document.getElementById("nail2").style.backgroundColor = color,
+        document.getElementById("nail3").style.backgroundColor = color,
+        document.getElementById("tile").style.backgroundColor = color;
 }
 
 function changeAdvice(object) {
     let text;
 
-    switch(object.id) {
+    switch (object.id) {
         case "firstAdvice": text = "If you are doing an uncut manicure, remove the burrs and push the cuticle away with an orange stick. Another option is to make an edged manicure and then maintain a neat appearance of the nail plate, regularly pushing the cuticle with an orange stick.";
-        break;
+            break;
         case "secondAdvice": text = "Massage improves the health of nails and makes them strong. A few drops of nourishing oil are enough for massage. Leave the oil on for a few minutes so that it starts to work, then treat your fingers with a cotton swab soaked in tonic.";
-        break;
+            break;
         case "thirdAdvice": text = "Before applying the nail polish, first go over the nail with a nutrient base (usually it is sold complete with varnish) and let it dry. This process is very important to protect the nail from the aggressive effects of harmful substances.";
-        break;
+            break;
         case "fourthAdvice": text = "Always wear latex or rubber gloves for housework. Many women consider it unnecessary to use them for housework and, in particular, for washing dishes. But this puts the nails in danger, for example, they can be scratched or broken.";
-        break;
+            break;
         default: text = "Unknown problem. Try reload this page and continue work. If not work after reload send massge for us manager.";
-        break;
+            break;
     }
 
     return document.getElementById("adviceText").innerHTML = text;
 }
 
-function sendEmail() {
+function showMenu() {
+    let pop = document.getElementById("burgerMenuFrameId");
+    let matchMediaQuery = window.matchMedia("(max-width: 639px)");
+    if (matchMediaQuery.matches) {
+        console
+        pop.style.cssText = "position: absolute; display: flex; flex-direction: column; flex-wrap: nowrap; gap: 10px;";
+    } else {
+        pop.style.cssText = "display: none; overflow: hidden;";
+    }
+    matchMediaQuery.addListener(showMenu);
+}
 
+function closeMenu() {
+    let pop = document.getElementById("burgerMenuFrameId");
+    pop.style.cssText = "display: none; overflow: hidden;";
+}
+
+function sendEmail() {
+    console.log("Sending");
 }
